a4e24bef52e134ce9339c216a6065945f6af07fa
Josh Cox <josh@webhosting.coop>
2016-01-04 15:17:32 -0600

    gitmodules.sh

---
+++ gitmodules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TMP_DIR=$(mktemp -d --suffix='.icingaweb2')
+cd $TMP_DIR
+
+# got this from here https://hub.docker.com/r/jordan/icinga2/~/dockerfile/
+# modified to use mktemp
+# Temporary hack to get icingaweb2 modules via git
+mkdir -p /etc/icingaweb2/enabledModules
+wget --no-cookies "https://github.com/Icinga/icingaweb2/archive/master.zip" -O $TMP_DIR/icingaweb2.zip
+unzip $TMP_DIR/icingaweb2.zip "icingaweb2-master/modules/doc/*" "icingaweb2-master/modules/monitoring/*" -d "$TMP_DIR/icingaweb2"
+cp -R $TMP_DIR/icingaweb2/icingaweb2-master/modules/monitoring /etc/icingaweb2/modules/
+cp -R  $TMP_DIR/icingaweb2/icingaweb2-master/modules/doc /etc/icingaweb2/modules/
+
+cd
+rm -Rf $TMP_DIR
