259109ee20dbe1b28d7c828cf921d78b4dd4eaf6
Josh Cox <josh@webhosting.coop>
2016-05-22 10:04:15 -0500

    slowdown

--- wait.sh
+++ wait.sh
@@ -5,5 +5,5 @@
     #sleep 0.1 # wait for 1/10 of the second before check again
 #done
 until docker exec icinga2-mysqltemp mysqladmin -hlocalhost -p`cat MYSQL_PASS` -uroot ping &>/dev/null; do
- echo -n "."; sleep 0.2
+ echo -n "."; sleep 2
 done
